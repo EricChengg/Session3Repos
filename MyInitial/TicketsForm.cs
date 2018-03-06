a7fe00dfd014c7cae2dbe1cbf55d8eaf613cafd9
Eric cheng <lung786@hotmail.com>
2018-03-06 20:59:44 +1030

    Initial loading of MyInitial.

---
+++ MyInitial/TicketsForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ticketing
+{
+    public partial class TicketsForm : Form
+    {
+        TicketPrice mTicketPrice;
+        int mSection = 2;
+        int mQuantity = 0;
+        bool mDiscount = false;
+        decimal discount = 5;
+
+
+        public TicketsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TicketsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCalculate_Click(object sender, EventArgs e)
+        {
+            mDiscount = false;
+            mQuantity = int.Parse(txtQuantity.Text);
+            discount = decimal.Parse(txtDiscount.Text);
+
+            if (chkDiscount.Checked)
+                { mDiscount = true; }
+
+            if (radBalcony.Checked)
+            {
+                mSection = 1;            
+            }
+            if (radGeneral.Checked)
+                { mSection = 2; }
+            if (radBox.Checked)
+                { mSection = 3; }
+
+            mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount, discount);
+
+            mTicketPrice.calculatePrice();
+            lblAmount.Text = System.Convert.ToString(mTicketPrice.AmountDue);
+        }
+    }
+}
